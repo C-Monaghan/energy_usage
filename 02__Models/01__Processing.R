54ec25f8c9aeebecc7dee73e0f93509544135da2
C-Monaghan <cormacmonaghan@tutanota.com>
2023-07-07 12:43:48 +0100

    Initial commit

---
+++ 02__Models/01__Processing.R
@@ -0,0 +1,24 @@
+# Data processing
+rm(list = ls())
+
+library(dplyr)
+library(lubridate) # For working with dates
+
+# Reading in data --------------------------------------------------------------
+path_data <- "./01__Data/01__Raw_Data/"
+
+usage_raw <- read.csv(file.path(path_data, "01__Data_Raw.csv"))
+
+# Processing -------------------------------------------------------------------
+usage <- usage_raw %>%
+  rename_with(~gsub("X", "", .), matches("^X\\d{2}\\.\\d{2}$")) %>% # Renaming columns
+  mutate(Date = as.Date(Date, format = "%d/%m/%Y"),
+         Day = wday(Date, label = TRUE),
+         Week = week(Date),
+         Month = month(Date, label = TRUE)) %>% # Extracting Day & Weeks & Months
+  select(Date, Day, Week, Month, everything(), -X) # Removing redundant last column
+
+# Exporting --------------------------------------------------------------------
+export_path <- "./01__Data/02__Processed_Data/"
+
+write.csv(file = file.path(export_path, "01__Usage_data.csv"), x = usage)
