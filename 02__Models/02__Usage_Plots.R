18591c74611ea298f6e65c405042fc24cd416ff1
C-Monaghan <cormacmonaghan@tutanota.com>
2024-07-02 09:52:56 +0100

    Update

--- 02__Models/02__Usage_Plots.R
+++ 02__Models/02__Usage_Plots.R
@@ -18,38 +18,55 @@ usage <- usage %>%
                                       "Fri", "Sat", "Sun")))
 
 # Plotting usage over time -----------------------------------------------------
-overall_usage <- ggplot(usage, aes(x = Timestamp, y = Usage)) +
+usage %>%
+  ggplot(aes(x = Timestamp, y = Usage)) +
   geom_line() +
   labs(title = "Energy Consumption Over Time",
-       x = "Timestamp", y = "Energy Consumption") +
-  theme_bw() +
-  theme(plot.title = element_text(hjust = 0.5))
+       x = "Timestamp", y = "Energy Consumption (Watts)") +
+  scale_x_datetime(date_breaks = "1 month", date_labels = "%b") +
+  theme_minimal() +
+  ggeasy::easy_center_title()
 
 # Plotting mean daily usage (by month) -----------------------------------------
 # Calculate the mean usage for each day and month
 mean_usage <- aggregate(Usage ~ Day + Month, data = usage, FUN = mean)
 
-mean_daily <- ggplot(data = mean_usage, aes(x = Day, y = Usage, fill = Month)) +
-  geom_bar(stat = "identity") +
+mean_usage %>%
+  ggplot(aes(x = Day, y = Usage, fill = Month)) +
+  geom_bar(stat = "identity", colour = "black") +
   facet_wrap(~ Month) +
-  labs(title ="Mean Daily Usage (by Month)", y = "Usage (Watts)") +
-  theme_bw() +
-  theme(legend.position = "none",
-        plot.title = element_text(hjust = 0.5))
+  labs(title ="Mean daily usage (by month)", y = "Usage (Watts)") +
+  theme_minimal() +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1),
+    strip.text = element_text(size = 12),
+    legend.position = "none",
+    panel.grid.major = element_line(color = "grey80"),
+    panel.grid.minor = element_blank()
+  ) +
+  ggeasy::easy_center_title() +
+  ggeasy::easy_remove_legend() +
+  colorspace::scale_fill_discrete_qualitative(palette = "Dynamic")
 
-mean_monthly <- ggplot(data = mean_usage, aes(x = Month, y = Usage, fill = Day)) +
-  geom_bar(stat = "identity") +
-  facet_wrap(~ Day) +
-  labs(title ="Mean Monthly Usage (by Day)", y = "Usage (Watts)") +
-  theme_bw() +
-  theme(legend.position = "none",
-        plot.title = element_text(hjust = 0.5))
-# Exporting --------------------------------------------------------------------
-export_path <- "./02__Models/results"
 
-cowplot::save_plot(file.path(export_path, "01__overall_usage.png"), plot = overall_usage,
-                   base_height = 10)
-cowplot::save_plot(file.path(export_path, "02__mean_daily_usage.png"), plot = mean_daily,
-                   base_height = 10)
-cowplot::save_plot(file.path(export_path, "03__mean_monthly_usage.png"), plot = mean_monthly,
-                   base_height = 10)
+weekly_usage <- aggregate(Usage ~ Week + Month, data = usage, FUN = mean)
+
+# Adjust Week column to week I moved in to the week I left
+weekly_usage <- weekly_usage %>%
+  mutate(Week = seq(1, length(Week), by = 1))
+
+weekly_usage %>%
+  ggplot(aes(x = Week, y = Usage)) +
+  geom_line(size = 1) +
+  geom_point(size = 3) +
+  labs(title = "Mean Weekly Energy Usage",
+       x = "Week",
+       y = "Average Energy Consumption (Watts)")  +
+  scale_x_continuous(breaks = seq(0, max(weekly_usage$Week), by = 5)) +  # Adjust x-axis breaks
+  theme_minimal(base_size = 15) +  
+  theme(
+    panel.grid.major = element_line(color = "grey80"),  # Add major grid lines
+    panel.grid.minor = element_blank(),  # Remove minor grid lines
+    axis.text.x = element_text(angle = 45, hjust = 1)  # Rotate x-axis labels
+  ) +
+  ggeasy::easy_center_title()
